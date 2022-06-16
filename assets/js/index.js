08f572bbcef53aab9b707f81be12422f8d93f193
liuwei <liuwei@qq.com>
2022-06-16 13:44:53 +0800

    主页布局完成

---
+++ assets/js/index.js
@@ -0,0 +1,41 @@
+$(function(){
+  //获取用户信息
+  getuserinfo()
+  //退出登录
+ $("#logout").on('click',()=>{
+  layer.confirm('确定退出吗', function(index){
+    localStorage.removeItem('token')
+   location.href='/login.html'
+    layer.close(index);
+  })
+
+ })
+
+})
+
+
+//获取用户信息函数
+function getuserinfo(){
+  $.ajax({
+    method:'GET',
+    url:'/my/userinfo',
+    success:(res)=>{
+      if(res.status!==0) return layui.layer.msg('获取用户信息失败')
+     //按需显示用户信息
+      showuserinfo(res.data)
+    },
+  })
+  //按需显示用户信息
+function showuserinfo(user){
+  if(user.user_pic===null){
+    $('.layui-nav-img').hide()
+    $('.text_avatar').show()
+  }
+  else{
+    $('.layui-nav-img').attr('src',user.user_pic).show()
+    $('.text_avatar').hide()
+  }
+  var name=user.nickname||user.username
+  $('#welcome').html('欢迎'+name)
+}
+}
